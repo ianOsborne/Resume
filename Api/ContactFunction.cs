d9a044aacc4886ba5bf5075f63391ace8d08c20e
Oz <ian.cosborne@yahoo.com>
2023-04-24 01:08:33 -0400

    Changed contact function to use environment variables for increased security

--- Api/ContactFunction.cs
+++ Api/ContactFunction.cs
@@ -3,7 +3,9 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Net.Mail;
+using System.Text;
 using System.Text.Json;
 using System.Text.Json.Nodes;
 using System.Threading.Tasks;
@@ -19,35 +21,30 @@ namespace ApiIsolated
 {
     public class ContactFunction
     {
-        private readonly ILogger _logger;
-
-        public ContactFunction(ILoggerFactory loggerFactory)
-        {
-            _logger = loggerFactory.CreateLogger<HttpTrigger>();
-        }
-
         [Function("ContactFunction")]
         public void Run(
-            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]
-            JsonObject req, ILogger log)
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)]
+            HttpRequestData req)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-            ContactMessage data = JsonSerializer.Deserialize<ContactMessage>(req);
-            SendMail(data);
+            SendMail(JsonSerializer.Deserialize<ContactMessage>(req.Body));
         }
+
 
         public void SendMail(ContactMessage contactMessage)
         {
-            SmtpClient smtpClient = new SmtpClient("email.ionos.com");
+            if (contactMessage == null)
+            {
+                throw new ArgumentNullException("Contact Message was null :(");
+            }
+            SmtpClient smtpClient = new SmtpClient("smtp.ionos.com");
             MailMessage message = new MailMessage();
             message.From = new MailAddress(contactMessage.Email);
             message.To.Add("mail@ianosborne.dev");
             message.Body = contactMessage.Name + " says " + contactMessage.Message;
             message.Subject = "Resume Website Contact";
             smtpClient.Port = 465;
-            smtpClient.Host = "smtp.ianosborne.dev";
             smtpClient.EnableSsl = true;
-            smtpClient.Credentials = new NetworkCredential("mail@ianosborne.dev", "2002@))@IAN");
+            smtpClient.Credentials = new NetworkCredential("mail@ianosborne.dev", Environment.GetEnvironmentVariable("MailPassword"));
             smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
             smtpClient.Send(message);
         }
